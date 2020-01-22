ac6b2ac388601dd339890998820bad1fdd9e4f32
IT-gaurav <56316270+IT-gaurav@users.noreply.github.com>
2020-01-21 19:03:22 -0800

    initial

---
+++ index.js
@@ -0,0 +1,5 @@
+const canvas = document.getElementsByClassName('playArea');
+const context = canvas.getContext('2d');
+
+context.fillStyle = '#000';
+context.fillReact(0, 0, canvas.width, canvas.height);
